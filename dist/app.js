f1903d75682d0fa4cb8fb74b3c4bfe9908d5b178
Shah Ehsan <shahehsan2002@gmail.com>
2024-07-05 11:01:23 +0600

    initial setup done

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
